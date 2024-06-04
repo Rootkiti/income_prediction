e8ed2023ed0296816fb905a008ddc288732ce72c
rootkiti <robertu20002000@gmail.com>
2024-06-04 17:45:57 +0200

    intitial commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from typing import Union
+from fastapi import FastAPI
+import pickle
+import pandas as pd
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+
+pickle_in = open('model.pkl','rb')
+model = pickle.load(pickle_in)
+
+app = FastAPI()
+
+# Add CORS middleware to allow all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+class data(BaseModel):
+    locality: str
+    sex: str
+    marital_status: str
+    sector: str
+    occupation: str
+    education_level: str
+    age_category: str
+    weekly_hours_category: str
+
+
+@app.post("/predict")
+def predict(features:data):
+    y =pd.DataFrame([{
+    'locality':features.locality,
+    'sex':features.sex,
+    'marital_status':features.marital_status,
+    'sector':features.sector,
+    'occupation':features.occupation,
+    'education_level':features.education_level,
+    'age_category':features.age_category,
+    'weekly_hours_category':features.weekly_hours_category
+    }])   
+
+    result = model.predict(y)[0]
+    return {"income_category": result}
